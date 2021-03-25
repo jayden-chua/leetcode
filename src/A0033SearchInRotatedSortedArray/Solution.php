99f0311da2ee7304dea3acbe94384821a05045c5
Jayden Chua <gjiehs@gmail.com>
2021-03-25 19:07:16 +0800

    add solutiong

--- src/A0033SearchInRotatedSortedArray/Solution.php
+++ src/A0033SearchInRotatedSortedArray/Solution.php
@@ -3,8 +3,81 @@
 namespace App\A0033SearchInRotatedSortedArray;
 
 class Solution {
-    
-    public function sample(): void {
-        return;
+
+    private $nums;
+
+    private $target;
+
+    /**
+     * @param $nums
+     * @param $target
+     * @return int
+     */
+    public function search($nums, $target) {
+        $numSize = count($nums);
+
+        $this->nums = $nums;
+        $this->target = $target;
+
+        $pivot = $this->findPivot(0, $numSize - 1);
+
+        if ($this->nums[$pivot] === $this->target) {
+            return $pivot;
+        }
+
+        if ($this->target < $this->nums[0]) {
+            return $this->searchRange($pivot, $numSize - 1);
+        }
+
+        return $this->searchRange(0, $pivot);
+    }
+
+    /**
+     * @param $left
+     * @param $right
+     * @return int
+     */
+    private function searchRange($left, $right) {
+        while ($left <= $right) {
+            $pivot = (int) (($left + $right) / 2);
+            if ($this->nums[$pivot] === $this->target) {
+                return $pivot;
+            }
+
+            if ($this->target < $this->nums[$pivot]) {
+                $right = $pivot - 1;
+            } else {
+                $left = $pivot + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @param $left
+     * @param $right
+     * @return int
+     */
+    private function findPivot($left, $right): int {
+        $pivot = 0;
+        if ($this->nums[$pivot] < $this->nums[count($this->nums) - 1]) {
+            return $pivot;
+        }
+
+        $pivot = (int) (($left + $right) / 2);
+
+        while ($left <= $right) {
+            if ($this->nums[$pivot] > $this->nums[$pivot+1]) {
+                return $pivot + 1;
+            } else {
+                if ($this->nums[$pivot] < $this->nums[$left]) {
+                    $right = $pivot - 1;
+                } else {
+                    $left = $pivot + 1;
+                }
+            }
+        }
+
+        return 0;
     }
 }
--- tests/A0033SearchInRotatedSortedArray/SolutionTest.php
+++ tests/A0033SearchInRotatedSortedArray/SolutionTest.php
@@ -7,15 +7,20 @@ final class SolutionTest extends TestCase
 {
     public function cases() {
         return [
-            [[12,345,2,6,7986], 2],
+            [[4,5,6,7,0,1,2], 0, 4],
+            [[4,5,6,7,0,1,2], 3, -1],
+            [[1], 0, -1],
         ];
     }
 
     /**
      * @dataProvider cases
-     */ 
-    public function testSample() {
+     * @param $input
+     * @param $target
+     * @param $expected
+     */
+    public function testSearch($input, $target, $expected) {
         $s = new Solution();
-        $this->assertEquals('', '');
+        $this->assertEquals($expected, $s->search($input, $target));
     }
 }
